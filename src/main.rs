fbd7c52dbe219f37c374dbb6adfe55c44716dedd
Wotori <10486621+wotori@users.noreply.github.com>
2022-09-29 01:03:39 +0300

    swagger

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,84 @@
 extern crate rocket;
 
 use rocket::tokio::time::{sleep, Duration};
+use rocket::{serde::json::Json};
 
+use rocket_okapi::okapi::schemars;
+use rocket_okapi::okapi::schemars::JsonSchema;
+use rocket_okapi::settings::UrlObject;
+use rocket_okapi::{openapi, openapi_get_routes, rapidoc::*, swagger_ui::*};
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+struct User {
+    user_id: u64,
+    username: String,
+    #[schemars(example = "example_email")]
+    email: Option<String>,
+}
+
+fn example_email() -> &'static str {
+    "test@example.com"
+}
+
+#[openapi(tag = "testing routes")]
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[openapi(tag = "testing routes")]
 #[get("/sleep/<seconds>")]
 async fn delay(seconds: u64) -> String {
     sleep(Duration::from_secs(seconds)).await;
     format!("Waited for {} seconds", seconds)
 }
 
+#[openapi(tag = "testing routes")]
+#[get("/user")]
+fn get_all_users() -> Json<Vec<User>> {
+    Json(vec![User {
+        user_id: 42,
+        username: "bob".to_owned(),
+        email: None,
+    }])
+}
+
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .mount("/", routes![index])
-        .mount("/test", routes![delay]).launch().await;
+        // .mount("/", routes![index])
+        .mount(
+            "/",
+            openapi_get_routes![
+                index,
+                delay,
+                get_all_users
+            ],
+        )
+        .mount(
+            "/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+        .mount(
+            "/rapidoc/",
+            make_rapidoc(&RapiDocConfig {
+                general: GeneralConfig {
+                    spec_urls: vec![UrlObject::new("General", "../openapi.json")],
+                    ..Default::default()
+                },
+                hide_show: HideShowConfig {
+                    allow_spec_url_load: false,
+                    allow_spec_file_load: false,
+                    ..Default::default()
+                },
+                ..Default::default()
+            }),
+        ).launch().await;
 }
 
